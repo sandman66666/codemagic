4dc2415a4811cf4891e1a2da9824fa658f67e1ca
טליה בן שבת <talya@gmail.com>
2025-03-26 11:31:32 +0200

    1

--- repository-ingest-example.py
+++ repository-ingest-example.py
@@ -13,17 +13,11 @@ Token count
 CLI tool: Run it as a shell command
 Python package: Import it in your code
 """
-#ingest repository from url:
-#summary, tree, content = gitingest.ingest("https://github.com/TalyaBsh/test1")
 
-#ingest local directory:
-temp_folder = "C:\\Users\\talya\\AppData\\Local\\Temp\\codeinsight"#this is an existing folder where system is cloning repository to.
-directory = os.listdir("C:\\Users\\talya\\AppData\\Local\\Temp\\codeinsight")
-print(directory)
-if directory:
-    summary, tree, content = gitingest.ingest(temp_folder + "\\" + directory[0])
-    """
-    saummary example:
+#ingest repository from url:
+summary, tree, content = gitingest.ingest("https://github.com/TalyaBsh/test1.git")
+"""
+    summary example:
     Repository: talyabsh/test1.git
     Files analyzed: 1
 
@@ -37,14 +31,23 @@ if directory:
     File: example-text.txt
     ================================================
     blah blah blah
-    """
+"""
 
-    with open(temp_folder + "\\summary.txt", 'w') as f:
-        f.write(summary)
-    with open(temp_folder + "\\tree.txt", 'w') as f:
-        f.write(tree)
-    with open(temp_folder + "\\content.txt", 'w') as f:
-        f.write(content)
+#ingest local directory:
+summary, tree, content = gitingest.ingest("path_to_directory/test1")
+"""
+    summary example:
+    Repository: path_to_directory/test1
+    Files analyzed: 1
 
+    tree example:
+    Directory structure:
+    └── path_to_director/test1/
+        └── example-text.txt
 
-
+    content example:
+    ================================================
+    File: example-text.txt
+    ================================================
+    blah blah blah
+"""
